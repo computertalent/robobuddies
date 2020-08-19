f9b71f212af7ebe84a1e284108d16c97e64b8626
propelahed <63991892+propelahed@users.noreply.github.com>
2020-08-19 14:06:55 +0900

    project files

---
+++ build/precache-manifest.eab10520317e8660a7e1d38426eb9ac7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcbff1a0df62be83f792872feac9645",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d9e351722e5c223a383b",
+    "url": "/robofriends/static/css/main.866a181d.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e351722e5c223a383b",
+    "url": "/robofriends/static/js/main.25386d9d.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/components/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ( {robots} ) => {
+	if (false){
+		throw new Error('NOOO!');
+	}
+
+	return (
+		<div>
+		{
+			robots.map((user,i)=> {
+				return(
+					<Card 
+						id={robots[i].id} 
+						name={robots[i].name} 
+						email={robots[i].email} 
+						key={robots[i].id}
+					/>
+				);
+			})
+		}
+		</div>
+	);
+}
+
+export default CardList;
